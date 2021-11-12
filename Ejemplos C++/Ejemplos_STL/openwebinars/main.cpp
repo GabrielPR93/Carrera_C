2efa2d092d9a8af083f78544bb95949409a40470
Gabriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-11-12 17:23:43 +0100

    STL

---
+++ Ejemplos C++/Ejemplos_STL/openwebinars/main.cpp
@@ -0,0 +1,25 @@
+#include "stl_examples.hpp"
+#include "smart_pointer.hpp"
+
+#include <iostream>
+
+// Se usa para incluir todos los elementos pertenicientes de std.
+// Nos ahorramos tener que escribir std::
+// Normalmente es mejor usar std:: pero para pruebas, se permite usarlo.
+
+using namespace std;
+
+int main()
+{
+    basicArrayExample();
+    vectorExample();
+    dequeExample();
+    listExample();
+    setExample();
+    mapExample();
+    smartPointers();
+
+    getchar();
+
+    return 0;
+}
